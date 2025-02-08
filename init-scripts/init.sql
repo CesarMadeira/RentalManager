ecc95d79cd75a7e6866b1d519fecc959b61e13a5
CesarMadeira <augusto.sts@hotmail.com>
2025-02-08 05:36:55 -0300

    script

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -6,8 +6,6 @@ CREATE TABLE motorcycle (
     model VARCHAR(100),
     year int
 );
-*/
-/*
 CREATE TABLE delivery_person (
     id VARCHAR(100),
     name VARCHAR(200),
@@ -17,21 +15,26 @@ CREATE TABLE delivery_person (
     document_type VARCHAR(2),
     document_image VARCHAR(100)
 );
-*/
-/*
-CREATE TABLE delivery_person (
+CREATE TABLE rent (
     id VARCHAR(100),
-    name VARCHAR(200),
-    cnpj VARCHAR(200),
-    date_of_birth date,
-    document_number VARCHAR(100),
-    document_type VARCHAR(2),
-    document_image VARCHAR(100)
+    delivery_person_id VARCHAR(100),
+    motorcycle_id VARCHAR(100),
+    start date,
+	finish date,
+	end_forecast date,
+    plan int
 );
 */
+
+
 -- delete from delivery_person
-select * from delivery_person
+select * from delivery_person;
 
 --delete from motorcycle
-select * from motorcycle
+select * from motorcycle;
+
+-- delete from rent
+select * from rent;
+
+
 
